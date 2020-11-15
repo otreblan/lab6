65f6c6f8c1d99073993b337e6dbc05703bcddc06
otreblan <otreblain@gmail.com>
2020-11-14 22:25:12 -0500

    Separate modules

---
+++ imem.v
@@ -0,0 +1,12 @@
+// vim: set expandtab:
+
+module imem (
+    a,
+    rd
+);
+    input wire [31:0] a;
+    output wire [31:0] rd;
+    reg [31:0] RAM [63:0];
+    initial $readmemh("memfile.dat", RAM);
+    assign rd = RAM[a[31:2]];
+endmodule
