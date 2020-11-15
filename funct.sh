67436fba30c4b8b989473056481e773ba65e5f97
otreblan <otreblain@gmail.com>
2020-11-14 22:47:54 -0500

    Add op

--- funct.sh
+++ funct.sh
@@ -11,5 +11,5 @@ arm-none-eabi-objdump -d a.out |\
 while IFS= read -rd '' i
 do
 	printf "%s " "$i"
-	dc -e 16i2o"$i"p | cut -b7-12
+	dc -e 16i2o"$i"p | cut -b5-6,7-12 --output-delimiter=" "
 done < <(hex) | paste "$@" - | column -to" "
